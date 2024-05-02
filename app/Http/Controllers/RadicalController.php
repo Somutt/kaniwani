7131141c4f8884b42c97163b804984bc2e97f2a5
Samuel Moura Alves <samuelmalves1@gmail.com>
2024-05-02 18:10:54 -0300

    levels, radicals and stages models and migrations

---
+++ app/Http/Controllers/RadicalController.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Radical;
+use Illuminate\Http\Request;
+
+class RadicalController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Radical $radical)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, Radical $radical)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Radical $radical)
+    {
+        //
+    }
+}
---
+++ database/migrations/2024_05_02_064510_create_radicals_table.php
@@ -0,0 +1,29 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('radicals', function (Blueprint $table) {
+            $table->id();
+            $table->char('ideogram', 1);
+            $table->foreignId('level_id')->constrained()->cascadeOnDelete();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('radicals');
+    }
+};
