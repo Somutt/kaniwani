c58b03301326c4c0a2e33e10ccaa58906acc025f
Samuel Moura <samuelmalves1@gmail.com>
2024-10-02 20:06:36 -0300

    welcome and dashboard foundation

--- resources/js/Pages/Welcome.tsx
+++ resources/js/Pages/Welcome.tsx
@@ -1,9 +1,42 @@
-import { Head } from '@inertiajs/react';
+import { PageProps } from '@/types';
+import { Head, Link } from '@inertiajs/react';
 
-export default function Welcome() {
+export default function Welcome({ auth }: PageProps) {
     return (
         <>
             <Head title='KaniWani'/>
+            <div className=' flex items-center justify-center min-h-screen bg-sky-900 text-slate-200'>
+                <main className='flex flex-col items-center'>
+                    <h1 className='text-4xl font-extrabold'>KaniWani</h1>
+                    <section className="p-10">
+                        <nav className="flex flex-1">
+                            {auth.user ? (
+                                <Link
+                                    href={route('dashboard')}
+                                    className="px-2 py-1 mr-1"
+                                >
+                                    Dashboard
+                                </Link>
+                            ) : (
+                                <>
+                                    <Link
+                                        href={route('login')}
+                                        className="px-3 py-2 mr-2 border-2"
+                                    >
+                                        Enter
+                                    </Link>
+                                    <Link
+                                        href={route('register')}
+                                        className='px-3 py-2 mr-2 border-2'
+                                    >
+                                        Register
+                                    </Link>
+                                </>
+                            )}
+                        </nav>
+                    </section>
+                </main>
+            </div>
         </>
     );
 }
